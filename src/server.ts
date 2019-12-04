226222a2be9c35260f37375aa1385b588acadb4b
ivoelbert <ivo.elbert1@gmail.com>
2019-12-04 19:13:54 -0300

    Added mutation with mocked storage

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { buildSchema, GraphQLSchema } from 'graphql';
 import * as express from 'express';
 import * as graphqlHTTP from 'express-graphql';
 
-const delayValue = <T>(time: number, value: T): Promise<T> => {
+const delayValue = <T>(time: number, value?: T): Promise<T> => {
     return new Promise(resolve => {
         setTimeout(() => resolve(value), time);
     });
@@ -14,16 +14,31 @@ export const buildServer = async (): Promise<void> => {
         type Query {
             hello: String
             echoMessage(msg: String!): String
+            numbers: [Int]
+        }
+
+        type Mutation {
+            pushNumber(num: Int!): [Int]
         }
     `);
 
+    const numbersArray: number[] = [];
+
     // The root provides a resolver function for each API endpoint
-    const hello = () => delayValue(2000, 'Hello world!');
+    const hello = () => delayValue(3000, 'Hello world!');
     const echoMessage = ({ msg }: { msg: string }) => delayValue(2000, msg);
+    const numbers = () => delayValue(1000, numbersArray);
+    const pushNumber = async ({ num }: { num: number }) => {
+        await delayValue(1000);
+        numbersArray.push(num);
+        return numbersArray;
+    };
 
     const root = {
         hello,
         echoMessage,
+        numbers,
+        pushNumber,
     };
 
     const app = express();
