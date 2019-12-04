69f4220bbb2a7264aa1cf58801cfc2b72a0e531e
ivoelbert <ivo.elbert1@gmail.com>
2019-12-04 15:09:46 -0300

    Basic schema

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { buildServer } from './server';
+
+buildServer();
---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import { buildSchema, GraphQLSchema } from 'graphql';
+import * as express from 'express';
+import * as graphqlHTTP from 'express-graphql';
+
+const delayValue = <T>(time: number, value: T): Promise<T> => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(value), time);
+    });
+};
+
+// Construct a schema, using GraphQL schema language
+export const buildServer = async (): Promise<void> => {
+    var schema: GraphQLSchema = buildSchema(`
+        type Query {
+            hello: String
+        }
+    `);
+
+    // The root provides a resolver function for each API endpoint
+    const hello = () => delayValue(2000, 'Hello world!');
+    const root = {
+        hello,
+    };
+
+    const app = express();
+
+    app.use(
+        '/graphql',
+        graphqlHTTP({
+            schema: schema,
+            rootValue: root,
+            graphiql: true,
+        })
+    );
+
+    app.listen(4000);
+    console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+};
